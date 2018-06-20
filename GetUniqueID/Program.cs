3583a72f8889ff8496dc2a09af1dd1b62c97a6d0
koao@home <koao8221@gmail.com>
2018-06-20 22:19:20 +0900

    Implementation

--- GetUniqueID/Program.cs
+++ GetUniqueID/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Management;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace GetUniqueID
 {
@@ -10,6 +10,53 @@ namespace GetUniqueID
     {
         static void Main(string[] args)
         {
+            string pass = "hoge";
+            string hash = GetMD5HashString(pass + GetVolumeSerials());
+            Console.WriteLine(hash);
+        }
+
+        /// <summary>
+        /// ストレージのシリアルナンバーを取得する。複数ある場合は連結して返す。
+        /// </summary>
+        /// <returns>シリアルナンバー</returns>
+        public static string GetVolumeSerials()
+        {
+            var volumes = new ManagementClass("Win32_DiskDrive").GetInstances();
+
+            string str = "";
+            foreach (var volume in volumes)
+            {
+                str += volume["SerialNumber"].ToString();
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// ハッシュ値を計算し、返す。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string GetMD5HashString(string text)
+        {
+            // 文字列をバイト型配列に変換する
+            byte[] data = Encoding.UTF8.GetBytes(text);
+
+            // MD5ハッシュアルゴリズム生成
+            var algorithm = new MD5CryptoServiceProvider();
+
+            // ハッシュ値を計算する
+            byte[] bs = algorithm.ComputeHash(data);
+
+            // リソースを解放する
+            algorithm.Clear();
+
+            // バイト型配列を16進数文字列に変換
+            var result = new StringBuilder();
+            foreach (byte b in bs)
+            {
+                result.Append(b.ToString("X2"));
+            }
+            return result.ToString();
         }
     }
 }
